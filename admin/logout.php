e53a9a6ec98a0b7783b093377d90535682a159a8
Muhammad Adeel Shakir <captainadeel000@gmail.com>
2024-08-30 05:44:32 +0500

    little changes made in query

--- admin/logout.php
+++ admin/logout.php
@@ -1,5 +1,22 @@
 <?php
-	session_start();
-	session_destroy();
-	header('Location:./signin.php');
+session_start();
+
+// Destroy all session data
+session_unset(); 
+session_destroy();
+
+// Optionally, you can also delete the session cookie if it exists
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, 
+        $params["path"], 
+        $params["domain"], 
+        $params["secure"], 
+        $params["httponly"]
+    );
+}
+
+// Redirect to login page
+header('Location: signin.php');
+exit();
 ?>
--- admin/signin.php
+++ admin/signin.php
@@ -1,6 +1,5 @@
 <?php
-    session_start();
-    require_once "./db-con.php";
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -45,43 +44,41 @@
         </div>
         <!-- Spinner End -->
 
-
         <!-- Sign In Start -->
         <div class="container-fluid">
             <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
                     <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
-                    <form class="form" method="POST" action="admin_login_query.php">
-                        <div class="d-flex align-items-center justify-content-between mb-3">
-                            <h3>Sign In</h3>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com">
-                            <label for="floatingInput">Email address</label>
-                        </div>
-                        <div class="form-floating mb-4">
-                            <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password">
-                            <label for="floatingPassword">Password</label>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-between mb-4">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        <form class="form" method="POST" action="admin_login_query.php">
+                            <div class="d-flex align-items-center justify-content-between mb-3">
+                                <h3>Sign In</h3>
                             </div>
-                            <a href="">Forgot Password</a>
-                        </div>
-                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4" name="submit">Sign In</button>
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com">
+                                <label for="floatingInput">Email address</label>
+                            </div>
+                            <div class="form-floating mb-4">
+                                <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-between mb-4">
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                </div>
+                                <a href="">Forgot Password</a>
+                            </div>
+                            <button type="submit" class="btn btn-primary py-3 w-100 mb-4" name="submit">Sign In</button>
                         </form>
-                            <?php
-                                
-                                if (isset($_SESSION['msg'])){
-                                    echo $_SESSION['msg'];
-                                    unset($_SESSION['msg']);
-                                
-                                }
-                                ?>
+
+                        <?php
+                        // Display session message if set
+                        if (isset($_SESSION['msg'])) {
+                            echo $_SESSION['msg'];
+                            unset($_SESSION['msg']);
+                        }
+                        ?>
                     </div>
-                    
                 </div>
             </div>
         </div>
